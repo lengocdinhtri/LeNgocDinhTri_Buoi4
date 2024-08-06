9d6b9c070b7861b9b7f40ee99e5574ca12449a20
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-06 23:23:50 +0700

    finish Bai1.3_BTTL

---
+++ Bai1/Bai1/Bai1.1.cpp
@@ -0,0 +1,40 @@
+﻿//#include <stdio.h>
+//
+//// Hàm tính tổng S(n) sử dụng đệ quy
+//int TinhTongDeQuy(int n) {
+//	if (n == 1) {
+//		return 1;
+//	}
+//	else {
+//		return n + TinhTongDeQuy(n - 1);
+//	}
+//}
+//
+//// Hàm tính tổng S(n) không sử dụng đệ quy (dùng vòng lặp)
+//int TinhTongKhongDeQuy(int n) {
+//	int Tong = 0;
+//	for (int i = 1; i <= n; i++) {
+//		Tong += i;
+//	}
+//	return Tong;
+//}
+//
+//int main() {
+//	int n;
+//
+//	printf("Nhap mot so nguyen duong: ");
+//	scanf("%d", &n);
+//
+//	if (n <= 0) {
+//		printf("So nhap vao phai la so nguyen duong !\n");
+//	}
+//	else {
+//		int KetQuaDeQuy = TinhTongDeQuy(n);
+//		int KetQuaKhongDeQuy = TinhTongKhongDeQuy(n);
+//
+//		printf("Tong S(%d) tinh bang de quy: %d\n", n, KetQuaDeQuy);
+//		printf("Tong S(%d) tinh khong dung de quy: %d\n", n, KetQuaKhongDeQuy);
+//	}
+//
+//	return 0;
+//}
---
+++ Bai1/Bai1/Bai1.3.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy để tính giá trị của S(n)
+double TinhS(int n) {
+	// Trường hợp cơ sở
+	if (n == 1) {
+		return 1.0;
+	}
+	else {
+		return TinhS(n - 1) + 1.0 / (2 * n + 1);
+	}
+}
+
+int main() {
+	int n;
+
+	printf("Nhap so nguyen duong n: ");
+	scanf("%d", &n);
+
+	double result = TinhS(n);
+
+	printf("Gia tri cua S(%d) la: %.6lf\n", n, result);
+
+	return 0;
+}
