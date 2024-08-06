c5959e361a8cce9b416062c6351c6c81d15253b1
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-06 23:48:11 +0700

    finish bai1.4_BTTL

---
+++ Bai1/Bai1.4.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy để tính giá trị của S(n)
+long long TinhS(int n) {
+	// Trường hợp cơ sở
+	if (n == 1) {
+		return 1 * 2;
+	}
+	else {
+		return TinhS(n - 1) + n * (n + 1);
+	}
+}
+
+int main() {
+	int n;
+
+	printf("Nhap gia tri duong n: ");
+	scanf("%d", &n);
+
+	long long result = TinhS(n);
+
+	printf("Gia tri cua S(%d) la: %lld\n", n, result);
+
+	return 0;
+}
