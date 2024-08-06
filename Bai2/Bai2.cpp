86db7af147315e9e1e01d49c3ac1551bf004b365
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-08-06 23:55:29 +0700

    finish Bai2_Tim uoc chung lon nhat cua 2 so nguyen duong a va b

---
+++ Bai2/Bai2.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy để tính ước chung lớn nhất (UCLN) của hai số nguyên dương a và b
+int TinhUCLN(int a, int b) {
+	// Trường hợp cơ sở: khi một trong hai số bằng 0
+	if (b == 0) {
+		return a;
+	}
+	if (a == 0) {
+		return b;
+	}
+	// Nếu a > b, thì tính UCLN(b, a - b)
+	if (a > b) {
+		return TinhUCLN(b, a - b);
+	}
+	else { // Nếu a <= b, thì tính UCLN(a, b - a)
+		return TinhUCLN(a, b - a);
+	}
+}
+
+int main() {
+	int a, b;
+
+	printf("Nhap so nguyen duong a: ");
+	scanf("%d", &a);
+	printf("Nhap so nguyen duong b: ");
+	scanf("%d", &b);
+
+	// Đảm bảo rằng a và b đều là số dương
+	if (a <= 0 || b <= 0) {
+		printf("Cac so nhap vao phai la so nguyen duong.\n");
+		return 1;
+	}
+
+	int result = TinhUCLN(a, b);
+
+	printf("Uoc chung lon nhat cua %d và %d la: %d\n", a, b, result);
+
+	return 0;
+}
